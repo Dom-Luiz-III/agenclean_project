dbb4a0a043ac05ab0eaecfef9c77cc2688e89cdc
Luiz Henrique <luizhenriqueccarvalho@gmail.com>
2023-12-06 17:45:07 -0300

    Página de Login (Primeira Versão)

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 
+//Cores:
 Color laranjaPrimario = const Color(0xFFFF671E);
 Color azulPrimario = const Color(0xFF01264E);
 Color azulSecundario = const Color(0xFF5D93F8);
 Color corEscura = const Color(0xFF22272B);
+Color corMaisEscura = const Color.fromARGB(255, 19, 21, 24);
 Color corBranca = const Color(0xFFE5E7E9);
+
+//Local para carregar imagens:
+Image logoImage = Image.asset('assets/images/agenclean_logo.png');
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -3,18 +3,227 @@ import 'package:agenclean_project/constants.dart';
 
 class LoginScreen extends StatefulWidget {
   @override
+  // ignore: library_private_types_in_public_api
   _LoginScreenState createState() => _LoginScreenState();
 }
 
+//Painel de login é chamado aqui
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: laranjaPrimario,
-      appBar: AppBar(
-        title: const Text('Agen Clean TTS'),
+      appBar: AppBar(),
+      body: const SignInPage(),
+    );
+  }
+}
 
+//Classe onde está o painel de login
+class SignInPage extends StatelessWidget {
+  const SignInPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final bool isSmallScreen = MediaQuery.of(context).size.width < 600;
+
+    return Scaffold(
+        backgroundColor: corEscura,
+        body: Center(
+            child: isSmallScreen
+                ? const Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      _Logo(),
+                      _FormContent(),
+                    ],
+                  )
+                : Container(
+                    padding: const EdgeInsets.all(32.0),
+                    constraints: const BoxConstraints(maxWidth: 800),
+                    child: const Row(
+                      children: [
+                        Expanded(child: _Logo()),
+                        Expanded(
+                          child: Center(child: _FormContent()),
+                        ),
+                      ],
+                    ),
+                  )));
+  }
+}
+
+//Essa classe é onde a logo junto com o texto abaixo dela 
+class _Logo extends StatelessWidget {
+  const _Logo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final bool isSmallScreen = MediaQuery.of(context).size.width < 600;
+
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Transform.scale(
+          scale: 0.7,
+          child: logoImage,
+        ),
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Text(
+            "Entre com a sua conta",
+            textAlign: TextAlign.center,
+            style: isSmallScreen
+                ? Theme.of(context)
+                    .textTheme
+                    .headlineSmall
+                    ?.copyWith(color: Colors.white)
+                : Theme.of(context)
+                    .textTheme
+                    .headlineMedium
+                    ?.copyWith(color: Colors.white),
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class _FormContent extends StatefulWidget {
+  const _FormContent({Key? key}) : super(key: key);
+
+  @override
+  State<_FormContent> createState() => __FormContentState();
+}
+
+//Classe onde ocorre o tratamento de envios do email e senha, além do design
+class __FormContentState extends State<_FormContent> {
+  bool _isPasswordVisible = false;
+  bool _rememberMe = false;
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      constraints: const BoxConstraints(maxWidth: 300),
+      child: Form(
+        key: _formKey,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextFormField(
+              validator: (value) {
+                // add email validation
+                if (value == null || value.isEmpty) {
+                  return 'Preencha este campo';
+                }
+
+                bool emailValid = RegExp(
+                        r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                    .hasMatch(value);
+                if (!emailValid) {
+                  return 'Por favor, coloque um email válido';
+                }
+
+                return null;
+              },
+              decoration: const InputDecoration(
+                labelText: 'Email',
+                hintText: 'Digite seu email',
+                prefixIcon: Icon(Icons.email_outlined, color: Colors.white),
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.white),
+                ),
+                labelStyle: TextStyle(color: Colors.white),
+                hintStyle: TextStyle(color: Colors.white),
+              ),
+              style: const TextStyle(color: Colors.white),
+            ),
+            _gap(),
+            TextFormField(
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Preencha este campo';
+                }
+
+                if (value.length < 6) {
+                  return 'A senha deve ter no mínimo 6 caracteres';
+                }
+                return null;
+              },
+              obscureText: !_isPasswordVisible,
+              decoration: InputDecoration(
+                labelText: 'Senha',
+                hintText: 'Coloque a sua senha',
+                prefixIcon:
+                    const Icon(Icons.lock_outline_rounded, color: Colors.white),
+                border: const OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.white),
+                ),
+                labelStyle: const TextStyle(color: Colors.white),
+                hintStyle: const TextStyle(color: Colors.white),
+                suffixIcon: IconButton(
+                  icon: Icon(
+                      _isPasswordVisible
+                          ? Icons.visibility_off
+                          : Icons.visibility,
+                      color: Colors.white),
+                  onPressed: () {
+                    setState(() {
+                      _isPasswordVisible = !_isPasswordVisible;
+                    });
+                  },
+                ),
+              ),
+              style: const TextStyle(color: Colors.white),
+            ),
+            _gap(),
+            CheckboxListTile(
+              value: _rememberMe,
+              onChanged: (value) {
+                if (value == null) return;
+                setState(() {
+                  _rememberMe = value;
+                });
+              },
+              title:
+                  const Text('Lembrar', style: TextStyle(color: Colors.white)),
+              controlAffinity: ListTileControlAffinity.leading,
+              dense: true,
+              contentPadding: const EdgeInsets.all(0),
+            ),
+            _gap(),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(4),
+                  ),
+                ),
+                child: const Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: Text(
+                    'Entrar',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                onPressed: () {
+                  if (_formKey.currentState?.validate() ?? false) {
+                    /// do something
+                  }
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
+
+  Widget _gap() => const SizedBox(height: 16);
 }
