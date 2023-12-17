1964974ce21a5ae7af1477380d1c078cc26be209
Ruan <ruanclaudio4@gmail.com>
2023-12-17 19:55:38 -0300

    fix main

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context){
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: ProfilePage(),
+      home: OnboardingPage(),
     );
   }
 }
