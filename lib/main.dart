bd994f2779b9bac82cd5247e6c0a249fcbe806da
Ruan <ruanclaudio4@gmail.com>
2023-12-17 20:23:14 -0300

    fix profile_screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:agenclean_project/screens/onboarding_page.dart';
 import 'package:flutter/material.dart';
-import 'package:agenclean_project/screens/profile_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() {
   runApp(const MainApp());
